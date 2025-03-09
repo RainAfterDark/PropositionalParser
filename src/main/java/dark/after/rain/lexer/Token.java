e41db21364ed717e064e3ef0b67da9bac5115d17
Ame <42061128+RainAfterDark@users.noreply.github.com>
2025-03-10 01:05:06 +0800

    Fix repeating expressions
    Should disregard index when comparing tokens

--- src/main/java/dark/after/rain/lexer/Token.java
+++ src/main/java/dark/after/rain/lexer/Token.java
@@ -1,8 +1,21 @@
 package dark.after.rain.lexer;
 
+import java.util.Objects;
+
 public record Token(TokenType type, char value, int index) {
     @Override
     public String toString() {
         return String.valueOf(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Token(TokenType type1, char value1, _))) return false;
+        return value() == value1 && type() == type1;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type(), value());
     }
 }
